fc6920b98c0174e392d881e85ce0c1740c092eb9
Marek <209477@edu.p.lodz.pl>
2019-03-13 16:21:53 +0100

    lol

---
+++ pekai/src/pekai/Pekai.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pekai;
+
+
+/**
+ * @author 209477
+ */
+public class Pekai extends Application {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
